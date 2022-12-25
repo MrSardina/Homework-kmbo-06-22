437537278d0e18ad855b71a281e74820d77f539c
Arslan <114141998+MrSardina@users.noreply.github.com>
2022-12-26 02:08:35 +0300

    Add files via upload

---
+++ 6/№6(2).jl
@@ -0,0 +1,30 @@
+
+using HorizonSideRobots
+
+size_left_right = 15
+size_top_bott = 15
+r=Robot(size_left_right,size_top_bott; animate=true)
+
+function perimeter_markers(r::Robot)
+    putmarker!(r)
+    while isborder(r, HorizonSide(3)) == false
+        move!(r, HorizonSide(3))
+    end
+    putmarker!(r)
+    
+    while isborder(r, HorizonSide(0)) == false
+        move!(r, HorizonSide(0))
+    end
+    putmarker!(r)
+    
+    while isborder(r, HorizonSide(1)) == false
+        move!(r, HorizonSide(1))
+    end
+    putmarker!(r)
+    
+    while isborder(r, HorizonSide(2)) == false
+        move!(r, HorizonSide(2))
+    end
+
+end 
+perimeter_markers(r)
